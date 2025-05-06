cbca40bcf0966667c15d029aeab1a28f9db77929
FredWhitman <caraldur@gmail.com>
2025-05-05 20:29:38 -0400

    Minor changes
    Made changes to background for a couple pages.

--- public/currentinventory.php
+++ public/currentinventory.php
@@ -12,7 +12,7 @@ $inventoryObj = new Inventory();
 ?>
 
 <!doctype html>
-
+<html lang="en"  data-bs-theme="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
